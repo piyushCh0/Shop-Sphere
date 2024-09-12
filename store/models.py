fa06f261279e23698638b041cfebba973dab37b0
Odd <piyushch6395@gmail.com>
2024-09-12 22:10:59 +0530

    fix customer DOB field

--- store/models.py
+++ store/models.py
@@ -39,8 +39,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
-    birth_date = models.DateField(
-        null=True, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
